93fad5c9433a02dcc0b71639c48a7cf8c9fa62f3
sanjib-asiwal <94754728+sanjib-asiwal@users.noreply.github.com>
2023-09-24 00:21:17 +0530

    Time: 29 ms (9.03%), Space: 30.8 MB (5.01%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -11,28 +11,29 @@
  */
 class Solution {
 public:
-    bool areSame(TreeNode* r1, TreeNode* r2){
-        if(!r1&&!r2) return true;
-        if(!r1 || !r2) return false;
-        return r1->val==r2->val &&
-        areSame(r1->left,r2->left) &&
-        areSame(r1->right,r2->right);
-    }
-    
-   void traverse(TreeNode* r1, TreeNode* r2,bool &ans){
-        if(areSame(r1,r2)){
-            ans=true;
+    void traverse(TreeNode* root, vector<int>& pre){
+        if(!root) {
+            pre.push_back(1e7);
             return;
         }
-        if(!r1) return;
-         traverse(r1->left,r2,ans);
-         traverse(r1->right,r2,ans);
+        pre.push_back(root->val);
+        traverse(root->left,pre);
+        traverse(root->right,pre);
     }
-    
-    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        bool ans=false;
-        traverse(root,subRoot,ans);
-        return ans;
         
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        vector<int> pre1,pre2;
+        traverse(root,pre1);
+        traverse(subRoot,pre2);
+        if(pre1.size()<pre2.size()) return false;
+        for(int i=0;i<=pre1.size()-pre2.size();i++){
+            int count=0;
+            for(int j=i;j<i+pre2.size();j++){
+                if(pre1[j]==pre2[j-i])
+                    count++;
+            }
+            if(count==pre2.size()) return true;
+        }
+        return false;
     }
 };
