3c5c8479829adfaf0d93907fad7f33e96663685b
sanjib-asiwal <94754728+sanjib-asiwal@users.noreply.github.com>
2023-09-26 15:20:58 +0530

    Time: 18 ms (14.63%), Space: 14.5 MB (21.93%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,33 +9,18 @@
  */
 class Solution {
 public:
-    vector<TreeNode*> path1,path2;
-    vector<TreeNode*> temp;
-    void CommonAncestor(TreeNode* root,TreeNode* p, TreeNode* q){
-        if(!root) return;
-        temp.push_back(root);
-        if(root->val==p->val){
-            for(int i=0;i<temp.size();i++){
-                path1.push_back(temp[i]);
-            }
-        }
-        if(root->val==q->val){
-            for(int i=0;i<temp.size();i++){
-                path2.push_back(temp[i]);
-            }
-        }
-        CommonAncestor(root->left,p,q);
-        CommonAncestor(root->right,p,q);
-        temp.pop_back();
+    TreeNode *CommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q){
+        if(!root) return NULL;
+        if(root->val==p->val || root->val==q->val) return root;
+        TreeNode* lst=CommonAncestor(root->left,p,q);
+        TreeNode* rst=CommonAncestor(root->right,p,q);
+        if(!lst) return rst;
+        if(!rst) return lst;
+        return root;
     }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(!root) return NULL;
-        CommonAncestor(root,p,q);
-        int i=0;
-        while(i<path1.size() && i<path2.size()){
-            if(path1[i] != path2[i]) break;
-            i++;
-        }
-        return path1[i-1];
+        TreeNode* ans=CommonAncestor(root,p,q);
+        return ans;
     }
 };
