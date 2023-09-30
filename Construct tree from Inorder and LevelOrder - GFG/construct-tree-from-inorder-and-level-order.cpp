8fc9cd14d7f02608daa0779e3002d7ffa2facd33
sanjib-asiwal <94754728+sanjib-asiwal@users.noreply.github.com>
2023-09-30 18:29:31 +0530

    Added solution - LeetHub

--- Construct tree from Inorder and LevelOrder - GFG/construct-tree-from-inorder-and-level-order.cpp
+++ Construct tree from Inorder and LevelOrder - GFG/construct-tree-from-inorder-and-level-order.cpp
@@ -58,20 +58,30 @@ struct Node
 };
 */
 Node* mybuildTree(int inorder[], int levelOrder[], int iStart, int iEnd,int n){
+        //base condition if left and right not exists
         if(iStart>iEnd) return NULL;
         Node* root=new Node(levelOrder[0]);
+        //base condition leaf node
         if(iStart==iEnd) return root;
         
+        //finding root (first node of levelOrder) in inorder and 
+        //idx will be index of that root 
         int idx=iStart;
         while(inorder[idx]!=root->key) idx++;
         
+        //all left from idx in inorder update in set
         unordered_set<int> set;
         for(int i=iStart;i<idx;i++) set.insert(inorder[i]);
         
+        //making new levelorder for left and right call
+        //in every call we are passing new level order 
+        //and for inorder we are just passing index
         int x=iEnd-iStart+1;
         int lLevel[set.size()] = {0};
         int rLevel[x-set.size()] = {0};
         int li=0,ri=-0;
+        //if the curr element in levelorder is in set it means it should be
+        //a part of new left levelorder  
         for(int i=1;i<iEnd-iStart+1;i++){
             if(set.find(levelOrder[i])!=set.end())
                lLevel[li++]=levelOrder[i];
@@ -85,7 +95,5 @@ Node* mybuildTree(int inorder[], int levelOrder[], int iStart, int iEnd,int n){
 }
 Node* buildTree(int inorder[], int levelOrder[], int iStart, int iEnd,int n)
 {
-//add code here.
 return mybuildTree(inorder,levelOrder,iStart,iEnd,n);
-
 }
