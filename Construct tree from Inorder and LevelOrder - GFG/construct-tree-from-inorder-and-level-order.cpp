d609c61939fd00925806ab69dba1984d9f83a037
sanjib-asiwal <94754728+sanjib-asiwal@users.noreply.github.com>
2023-09-30 18:19:33 +0530

    Added solution - LeetHub

---
+++ Construct tree from Inorder and LevelOrder - GFG/construct-tree-from-inorder-and-level-order.cpp
@@ -0,0 +1,91 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+struct Node
+{
+	int key;
+	struct Node *left;
+	struct Node *right;
+	
+	Node(int x){
+	    key = x;
+	    left = NULL;
+	    right = NULL;
+	}
+};
+
+Node* buildTree(int inorder[], int levelOrder[], int iStart, int iEnd,int n);
+void printPreorder(Node* node)
+{
+    if (node == NULL)
+       return;
+    cout << node->key << " ";
+    printPreorder(node->left);
+    printPreorder(node->right);
+    
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+    int n;
+    cin>>n;
+    int in[n],level[n];
+    for(int i=0;i<n;i++){
+        cin>>in[i];
+    }
+    for(int i=0;i<n;i++){
+        cin>>level[i];
+    }
+    Node *root=NULL;
+    root = buildTree(in, level, 0, n - 1,n);
+    printPreorder(root);
+    cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+/*Complete the function below
+Node is as follows:
+struct Node
+{
+    int key;
+    struct Node* left, *right;
+};
+*/
+Node* mybuildTree(int inorder[], int levelOrder[], int iStart, int iEnd,int n){
+        if(iStart>iEnd) return NULL;
+        Node* root=new Node(levelOrder[0]);
+        if(iStart==iEnd) return root;
+        
+        int idx=iStart;
+        while(inorder[idx]!=root->key) idx++;
+        
+        unordered_set<int> set;
+        for(int i=iStart;i<idx;i++) set.insert(inorder[i]);
+        
+        int x=iEnd-iStart+1;
+        int lLevel[set.size()] = {0};
+        int rLevel[x-set.size()] = {0};
+        int li=0,ri=-0;
+        for(int i=1;i<iEnd-iStart+1;i++){
+            if(set.find(levelOrder[i])!=set.end())
+               lLevel[li++]=levelOrder[i];
+            else
+                rLevel[ri++]=levelOrder[i];
+        }
+        
+        root->left=mybuildTree(inorder,lLevel,iStart,idx-1,n);
+        root->right=mybuildTree(inorder,rLevel,idx+1,iEnd,n);
+        return root;
+}
+Node* buildTree(int inorder[], int levelOrder[], int iStart, int iEnd,int n)
+{
+//add code here.
+return mybuildTree(inorder,levelOrder,iStart,iEnd,n);
+
+}
